1613b2ab6938260f5dc86384ec20e2fdbb1bc2bd
BigHeadliner <bigheadliner@gmail.com>
2022-08-03 15:50:04 +0300

    video fashion

--- gulpfile.js
+++ gulpfile.js
@@ -34,7 +34,8 @@ function browsersync() {
  function scripts() { 
    return src([   
     'node_modules/jquery/dist/jquery.js', 
-    'node_modules/slick-carousel/slick/slick.js',
+    'node_modules/slick-carousel/slick/slick.js', 
+    'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
      'app/js/main.js'
    ]) 
    .pipe(concat('main.min.js')) 
